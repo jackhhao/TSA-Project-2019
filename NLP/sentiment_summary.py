622773fc726ccbceb44b169dc6b3a1c8e3e4a7a7
Jack <36602317+jackhhao@users.noreply.github.com>
2019-03-31 16:05:45 -0400

    commit

--- NLP/sentiment_summary.py
+++ NLP/sentiment_summary.py
@@ -1,10 +1,21 @@
 import nlp_summary
 import sentiment_analysis
 import io
+while True:
+	try:	
+		i = input("url: ")
 
-i = input("url: ")
+		"""
+		with open("pageContent.txt", "w") as f:
+			f.write(sentiment_analysis.main(i))
 
-with open("pageContent.txt", "w") as f:
-	f.write(sentiment_analysis.main(i))
+		nlp_summary.generate_summary("pageContent.txt", len(sentiment_analysis.main(i))//3)
+		"""
 
-nlp_summary.generate_summary("pageContent.txt", len(sentiment_analysis.main(i))/3.0)
+		sentiment_analysis.main(i)
+		cont = input("cont? ")
+		if cont == "no":
+			break
+	except KeyboardInterrupt:
+		print("sorry, don't")
+		break
